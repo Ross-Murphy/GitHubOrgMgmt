dc9a990deeaefc15de9e60ddbcabeb0473188ed7
Ross Murphy <Ross.Murphy@sait.ca>
2024-07-12 13:39:25 -0600

    allo spec og from env var

--- set_token.sh
+++ set_token.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-read -s -p "GitHub Access Token:" GITHUB_PRIVATE_TOKEN 
+
+read -p "GitHub Organization Name:" GITHUB_ORG_NAME
+export GITHUB_ORG_NAME
+
+read -s -p "GitHub Access Token:" GITHUB_PRIVATE_TOKEN
 export GITHUB_PRIVATE_TOKEN
+
+printf "\n" # Let's have a new line
